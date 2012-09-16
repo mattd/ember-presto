0ec1d6117510cf9b0d4c3a6508d44e39e28998eb
Matt Dawson <matthewtdawson@gmail.com>
2012-09-15 22:10:39 -0400

    Add transitions files.

---
+++ test/my_test.js
@@ -0,0 +1,9 @@
+// Write your [mocha](http://visionmedia.github.com/mocha/) specs here.
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
